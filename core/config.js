d1aa145bfbe20cfe978437c95382d94c3b0394d2
vinagar <vineeth61@gmail.com>
2017-02-19 17:13:15 +0530

    Correcting error

--- core/config.js
+++ core/config.js
@@ -1,15 +1,18 @@
 
-module.config(["$stateProvider","$urlRouterProvider",function ($stateProvider,$urlRouterProvider){
+module.config(["$stateProvider","$urlRouterProvider",config]);
+
+function config($stateProvider,$urlRouterProvider){
 
 	$urlRouterProvider.otherwise('/');
 
     $stateProvider
 
         .state('home', {
-            url: '/'
+            url: '/',
+			templateUrl : 'core/home.html'
         })
 
         .state('about', {
             // we'll get to this in a bit       
         });
-}]);
+}
